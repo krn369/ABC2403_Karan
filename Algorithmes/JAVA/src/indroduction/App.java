2ad05f84fe09433c2b73e07dc4a01f8a11c9c6d2
krn369 <144631087+krn369@users.noreply.github.com>
2024-04-24 14:52:33 +0200

    update

---
+++ Algorithmes/JAVA/src/indroduction/App.java
@@ -0,0 +1,44 @@
+package indroduction;
+
+public class App {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("HELLO WORLD !"); //Affiche "Hello World " dans la console.
+		System.out.println("HELLO à toute le monde !!");
+		
+		// VARIABLES
+		
+		String prenom; // Déclaration d'une variable "prenom" de type "chaine de caractères"
+		
+		
+		int age; // Déclaration d'une variable "age" de type "Nombre entier" (valeur scalaire)
+		// (place de valeurs d'un INT (-2 147 000 000 à +2 147 000 000))
+		
+		
+		long soldBancaire; // Déclaration d'une variable "soldBancaire" de type "Nombre entier 64 bits" (valeur scalaire)
+		
+		
+		float tailleEnMetres; //Déclaration d'une variable "tailleEnMetres" de type "Nombre réel 32 bits" (Valeur scalaire)
+		
+		
+		double tailleEnMetresPourLesTresGrands; // Déclaration d'une varaiable "tailleEnMetresPourTresGrands" de type "Nombre réel 64bits" (valeur scalaire)
+		
+		
+		Boolean estMajeur; // Déclaration d'une variable "estMajeur" de type "Booléen" (Valeur scalaire)
+		
+		
+		prenom = "Karan"; // Affection d'une valeur à la variable "prenom"
+		
+				
+		prenom = "Rojita"; 	// modification de la valeur de la variable "prenom"
+		
+
+		System.out.println("Bonjour " + prenom);
+		
+		
+		
+	}
+
+}
