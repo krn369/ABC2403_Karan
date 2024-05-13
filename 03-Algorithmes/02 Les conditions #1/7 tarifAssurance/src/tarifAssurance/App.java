defb4039c37aee569ae754e741a82972326ca923
krn369 <144631087+krn369@users.noreply.github.com>
2024-05-13 16:36:02 +0200

    copied but you need to understand it well.

--- 03-Algorithmes/02 Les conditions #1/7 tarifAssurance/src/tarifAssurance/App.java
+++ 03-Algorithmes/02 Les conditions #1/7 tarifAssurance/src/tarifAssurance/App.java
@@ -5,24 +5,69 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		
-		int age,tempsPermis,tempsContrat,nbAccidents; 
-		String couleurTarifFidelite,couleurTarif,ancienneContratType;
-		
-		Scanner sc = new Scanner(System.in);
-		
-		// Demander les données à l'utilisateur
-	    System.out.print("Saisissez l'âge du conducteur : ");
-	    age = sc.nextInt();
-	    System.out.print("Saisissez l'ancienneté du permis (en années) : ");
-	    tempsPermis = sc.nextInt();
-	    System.out.print("Saisissez le nombre d'accidents responsables : ");
-	    nbAccidents = sc.nextInt();
-	    System.out.print("Saisissez l'ancienneté du contrat type en couler : ");
-	    ancienneContratType = sc.next();
-	    System.out.print("Saisissez l'ancienneté du contrat type en couler : ");
-	    tempsContrat = sc.nextInt();
-	    
-	}
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Entrez l'âge : ");
+        int age = sc.nextInt();
+
+        System.out.print("Entrez les années de permis : ");
+        int anneesPermis = sc.nextInt();
+
+        System.out.print("Nombre d'accidents : ");
+        int nombreAccidents = sc.nextInt();
+
+        System.out.print("Années en tant que client : ");
+        int anneesClient = sc.nextInt();
+
+        String tarifInitial = determinerTarifInitial(age, anneesPermis, nombreAccidents);
+        String tarifFinal = appliquerReductionFidelité(tarifInitial, anneesClient);
+
+        System.out.println("Tarif initial : " + tarifInitial);
+        System.out.println("Tarif final : " + tarifFinal);
+    }
+
+    private static String determinerTarifInitial(int age, int anneesPermis, int nombreAccidents) {
+        if (age < 25 && anneesPermis < 2) {
+            if (nombreAccidents == 0) {
+                return "Rouge";
+            } else {
+                return "Refusé";
+            }
+        } else if (age < 25 || anneesPermis < 2) {
+            if (nombreAccidents == 0) {
+                return "Orange";
+            } else if (nombreAccidents == 1) {
+                return "Rouge";
+            } else {
+                return "Refusé";
+            }
+        } else {
+            if (nombreAccidents == 0) {
+                return "Vert";
+            } else if (nombreAccidents == 1) {
+                return "Orange";
+            } else if (nombreAccidents == 2) {
+                return "Rouge";
+            } else {
+                return "Refusé";
+            }
+        }
+    }
+
+    private static String appliquerReductionFidelité(String tarifInitial, int anneesClient) {
+        if (anneesClient > 5) {
+            switch (tarifInitial) {
+                case "Vert":
+                    return "Bleu";
+                case "Orange":
+                    return "Vert";
+                case "Rouge":
+                    return "Orange";
+                default:
+                    return tarifInitial;
+            }
+        } else {
+            return tarifInitial;
+        }
+    }
 }
