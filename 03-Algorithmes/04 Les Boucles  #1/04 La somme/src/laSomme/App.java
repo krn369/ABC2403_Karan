e681d9ddd751907b1e1a6d993f364de0f73e8b2f
krn369 <144631087+krn369@users.noreply.github.com>
2024-05-22 12:46:17 +0200

    1

--- 03-Algorithmes/04 Les Boucles  #1/04 La somme/src/laSomme/App.java
+++ 03-Algorithmes/04 Les Boucles  #1/04 La somme/src/laSomme/App.java
@@ -7,24 +7,24 @@ public class App {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
-	    // Déclaration des variables
-        int nombreLimite;
-        int somme = 0;
 
-        // Demander le nombre limite à l'utilisateur
-        System.out.print("Saisissez un nombre limite: ");
-        nombreLimite = sc.nextInt();
+		// Déclaration des variables
+		int nombreLimite;
+		int somme = 0;
 
-        // Calcul de la somme des entiers jusqu'à nombreLimite
-        for (int i = 1; i <= nombreLimite; i++) {
-            somme += i;
-        }
+		// Demander le nombre limite à l'utilisateur
+		System.out.print("Saisissez un nombre limite: ");
+		nombreLimite = sc.nextInt();
 
-        // Affichage de la somme calculée
-        System.out.println("La somme des nombres jusqu'à " + nombreLimite + " est : " + somme);
-	
-        sc.close();
+		// Calcul de la somme des entiers jusqu'à nombreLimite
+		for (int i = 1; i <= nombreLimite; i++) {
+			somme += i;
+		}
+
+		// Affichage de la somme calculée
+		System.out.println("La somme des nombres jusqu'à " + nombreLimite + " est : " + somme);
+
+		sc.close();
 
 	}
 
