2b14fc6256d757bda3966a04037bd0138bbc64df
krn369 <144631087+krn369@users.noreply.github.com>
2024-06-13 15:50:30 +0200

    1

--- 04 Extra/object/bouteille/src/bouteille/App.java
+++ 04 Extra/object/bouteille/src/bouteille/App.java
@@ -4,15 +4,9 @@ public class App {
 
 	public static void main(String[] args) {
 
+		// Make a few bottles
+
 		Bouteille bouteille1 = new Bouteille();
-
-		float a = 1.2f;
-
-		Bouteille bouteille2 = new Bouteille(a, 0.8f, true, "H2O");
-
-		Bouteille bouteille3 = new Bouteille(bouteille2);
-
-		float contenanceEnClDeBouteille2 = bouteille2.donneContenanceEnCL();
 
 	}
 
--- 04 Extra/object/bouteille/src/bouteille/Bouteille.java
+++ 04 Extra/object/bouteille/src/bouteille/Bouteille.java
@@ -4,17 +4,17 @@ public class Bouteille {
 
 	// attributes
 	// Properties of the bottle
-	
+
 	private float contenanceEnL; // How much liquid it can hold (litres)
-	private float contenuEnL;    // How much liquid is in it now (litres) duplicate variable name fixed
-	private boolean ouvert;       // Whether the lid is on or off ( true for open, false for close)
-	private String typeDeContenu;  // What kind of the drink is in it (water, juice, etc)
+	private float contenuEnL; // How much liquid is in it now (litres) duplicate variable name fixed
+	private boolean ouvert; // Whether the lid is on or off ( true for open, false for close)
+	private String typeDeContenu; // What kind of the drink is in it (water, juice, etc)
 
 	// access/modifier
 
 	// constructor
 
-	// constructeur par default : Makes a regular bottle 
+	// constructeur par default : Makes a regular bottle
 
 	public Bouteille() {
 		contenanceEnL = 1;
@@ -24,7 +24,7 @@ public class Bouteille {
 
 	}
 
-	// Constructor with arguments : Lets you define specific properties 
+	// Constructor with arguments : Lets you define specific properties
 
 	public Bouteille(float _contenanceEnL, float _contenuEnL, boolean _ouvert, String _typeDeContenu) {
 		this.contenanceEnL = _contenanceEnL;
@@ -45,7 +45,7 @@ public class Bouteille {
 	}
 
 	// Methods
-	// Check size : See how big the bottle is (in units of 100 milliliters) 
+	// Check size : See how big the bottle is (in units of 100 milliliters)
 
 	public float donneContenanceEnCL() {
 
@@ -55,52 +55,50 @@ public class Bouteille {
 		return result;
 	}
 
-		// See if Full : Check if the bottle has all the liquid it can hold
-	
+	// See if Full : Check if the bottle has all the liquid it can hold
+
 	public boolean estPlien() {
 		return this.contenuEnL == this.contenanceEnL;
-		
+
 	}
-	
+
 	// Close the Lid : Try to put the lid on the bottle.
-	
+
 	public boolean fermer() {
-		
-		if(this.ouvert) {
+
+		if (this.ouvert) {
 			this.ouvert = false;
-		} 
+		}
 		return this.ouvert;
 	}
-	
-	
+
 	// Open the Lid : Try to take the lid off the bottle.
-	
+
 	public boolean ouvrir() {
 		if (!this.ouvert) {
 			this.ouvert = true;
 		}
 		return this.ouvert;
-			
+
 	}
-	
+
 	// Add Drink : Try to pour more drink into the bottle
-	
+
 	public boolean remplir(float remplirICI) {
-		
+
 		// traitment
 
 		if (this.ouvert && this.contenanceEnL - this.contenuEnL >= remplirICI) {
 
-			
-			this.contenuEnL +=  remplirICI;
+			this.contenuEnL += remplirICI;
 			return true;
-		} 
+		}
 		return false;
 
 	}
-	
+
 	// Take Out Drink : Try to pour some drink out of the bottle
-	
+
 	public boolean vider(float videzICI) {
 		if (this.ouvert && this.contenuEnL >= videzICI) {
 			this.contenuEnL -= videzICI;
@@ -108,23 +106,23 @@ public class Bouteille {
 		}
 		return false;
 	}
-	
-	  // Fill Completely: Try to fill the bottle all the way up
-	  public boolean remplireComplet() {
-	    if (this.ouvert && this.contenuEnL != this.contenanceEnL) {
-	      this.contenuEnL = this.contenanceEnL;
-	      return true;
-	    }
-	    return false;
-	  }
 
-	  // Empty Completely: Try to pour out all the drink in the bottle
-	  public boolean videzComplet() {
-	    if (this.ouvert && this.contenuEnL != 0) {
-	      this.contenuEnL = 0;
-	      return true;
-	    }
-	    return false;
-	  }
+	// Fill Completely: Try to fill the bottle all the way up
+	public boolean remplireComplet() {
+		if (this.ouvert && this.contenuEnL != this.contenanceEnL) {
+			this.contenuEnL = this.contenanceEnL;
+			return true;
+		}
+		return false;
+	}
+
+	// Empty Completely: Try to pour out all the drink in the bottle
+	public boolean videzComplet() {
+		if (this.ouvert && this.contenuEnL != 0) {
+			this.contenuEnL = 0;
+			return true;
+		}
+		return false;
+	}
 
 }
