94bf22920497f57b21f2d77c376cab783678ce7b
KARAN PARIYAR <144631087+krn369@users.noreply.github.com>
2024-06-25 00:13:11 +0200

    Update Bouteille.java

--- 04 Extra/object/bouteille/src/bouteille/Bouteille.java
+++ 04 Extra/object/bouteille/src/bouteille/Bouteille.java
@@ -14,6 +14,8 @@ public class Bouteille {
     private String typeDeContenu; // What kind of drink is in it (water, juice, etc.)
 
     // Constructor with no arguments: Makes a regular bottle
+    // This constructor initializes a new bottle with a capacity of 1 liter, filled with 1 liter of water, and the bottle is closed.
+    
     public Bouteille() {
         this.contenanceEnL = 1;
         this.contenuEnL = 1;
@@ -22,6 +24,8 @@ public class Bouteille {
     }
 
     // Constructor with arguments: Lets you define specific properties
+    // This constructor allows you to create a bottle with specific values for capacity, current amount of liquid, open/closed state, and type of liquid.
+    
     public Bouteille(float contenanceEnL, float contenuEnL, boolean ouvert, String typeDeContenu) {
         this.contenanceEnL = contenanceEnL;
         this.contenuEnL = contenuEnL;
@@ -38,17 +42,24 @@ public class Bouteille {
     }
 
     // Methods
+    
     // Check size: See how big the bottle is (in units of 100 milliliters)
+    // This method returns the bottle's capacity in centiliters (100 milliliters = 1 centiliter).
+    
     public float donneContenanceEnCL() {
         return this.contenanceEnL * 100;
     }
 
     // See if Full: Check if the bottle has all the liquid it can hold
+    // This method checks if the bottle is full (returns true if it is full, false otherwise).
+    
     public boolean estPlein() {
         return this.contenuEnL == this.contenanceEnL;
     }
 
     // Close the Lid: Try to put the lid on the bottle.
+    // This method closes the bottle if it is open and returns true if it is closed.
+    
     public boolean fermer() {
         if (this.ouvert) {
             this.ouvert = false;
@@ -57,6 +68,8 @@ public class Bouteille {
     }
 
     // Open the Lid: Try to take the lid off the bottle.
+    // This method opens the bottle if it is closed and returns true if it is open.
+    
     public boolean ouvrir() {
         if (!this.ouvert) {
             this.ouvert = true;
@@ -65,6 +78,8 @@ public class Bouteille {
     }
 
     // Add Drink: Try to pour more drink into the bottle
+    // This method adds a specified amount of liquid to the bottle if it is open and has enough space. It returns true if the bottle was successfully filled.
+    
     public boolean remplir(float quantite) {
         if (this.ouvert && this.contenanceEnL - this.contenuEnL >= quantite) {
             this.contenuEnL += quantite;
@@ -74,6 +89,8 @@ public class Bouteille {
     }
 
     // Take Out Drink: Try to pour some drink out of the bottle
+    // This method removes a specified amount of liquid from the bottle if it is open and has enough liquid. It returns true if the bottle was successfully emptied.
+    
     public boolean vider(float quantite) {
         if (this.ouvert && this.contenuEnL >= quantite) {
             this.contenuEnL -= quantite;
@@ -83,6 +100,8 @@ public class Bouteille {
     }
 
     // Fill Completely: Try to fill the bottle all the way up
+    // This method fills the bottle to its maximum capacity if it is open and not already full. It returns true if the bottle was successfully filled.
+    
     public boolean remplirComplet() {
         if (this.ouvert && this.contenuEnL != this.contenanceEnL) {
             this.contenuEnL = this.contenanceEnL;
@@ -92,6 +111,7 @@ public class Bouteille {
     }
 
     // Empty Completely: Try to pour out all the drink in the bottle
+    // This method empties all the liquid from the bottle if it is open and not already empty. It returns true if the bottle was successfully emptied.
     public boolean viderComplet() {
         if (this.ouvert && this.contenuEnL != 0) {
             this.contenuEnL = 0;
