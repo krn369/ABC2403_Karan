750fb3e8f57dd34b2936f5c80f47c30b096a5ae2
KARAN PARIYAR <144631087+krn369@users.noreply.github.com>
2024-06-25 00:41:14 +0200

    Update App.java

--- 04 Extra/object/bouteille/src/bouteille/App.java
+++ 04 Extra/object/bouteille/src/bouteille/App.java
@@ -5,30 +5,33 @@ public class App {
 	public static void main(String[] args) {
 
         // Create a Coca bottle
+	// A bottle of Coca is created with a capacity of 1.5 liters, currently holding 1 liter of Coca, and it is initially closed.
+	// A message is printed to confirm the creation of the Coca bottle.
+		
         Bouteille coca = new Bouteille(1.5f, 1.0f, false, "Coca");
-        System.out.println("Coca bottle created with " + coca.contenuEnL + "L of " + coca.typeDeContenu);
+        System.out.println("Bouteille de Coca créée avec " + coca.contenuEnL + "L de " + coca.typeDeContenu);
 
         // Open the Coca bottle and fill it completely
         coca.ouvrir();
         coca.remplirComplet();
-        System.out.println("Coca bottle is now filled to " + coca.contenuEnL + "L");
+        System.out.println("La bouteille de Coca est maintenant remplie à " + coca.contenuEnL + "L");
 
         // Create a H2O bottle
         Bouteille h2o = new Bouteille();
-        System.out.println("H2O bottle created with " + h2o.contenuEnL + "L of " + h2o.typeDeContenu);
+        System.out.println("Bouteille H2O créée avec " + h2o.contenuEnL + "L de " + h2o.typeDeContenu);
 
         // Open the H2O bottle and empty it completely
         h2o.ouvrir();
         h2o.viderComplet();
-        System.out.println("H2O bottle is now emptied to " + h2o.contenuEnL + "L");
+        System.out.println("La bouteille H2O est maintenant vidée " + h2o.contenuEnL + "L");
 
         // Create a wine bottle
         Bouteille wine = new Bouteille(0.75f, 0.75f, true, "Wine");
-        System.out.println("Wine bottle created with " + wine.contenuEnL + "L of " + wine.typeDeContenu);
+        System.out.println("Bouteille de vin créée avec " + wine.contenuEnL + "L de " + wine.typeDeContenu);
 
         // Empty some wine from the bottle
         wine.vider(0.25f);
-        System.out.println("Wine bottle now contains " + wine.contenuEnL + "L after pouring out 0.25L");	
+        System.out.println("La bouteille de vin contient désormais " + wine.donneContenanceEnCL() + "CL après avoir versé 25 CL");	
 
 	}
 
