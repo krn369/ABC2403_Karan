f1af8119324e475545e6515c969edb32798d973e
KARAN PARIYAR <144631087+krn369@users.noreply.github.com>
2024-06-25 00:55:13 +0200

    Update Bureau.java

--- 04 Extra/object/Bureau électrique/src/bureauElectrique/Bureau.java
+++ 04 Extra/object/Bureau électrique/src/bureauElectrique/Bureau.java
@@ -1,7 +1,40 @@
 package bureauElectrique;
 
-public class Bureau {
+import java.math.BigDecimal;
 
-	
-	// Base class for BureauElectrique.
+public class Bureau {
+    protected BigDecimal hauteurEnCM;
+    protected BigDecimal largeurEnCM;
+    protected BigDecimal longueurEnCM;
+
+    // Default constructor
+    public Bureau() {}
+
+    // Parameterized constructor
+    public Bureau(BigDecimal largeurEnCM, BigDecimal longueurEnCM, BigDecimal hauteurEnCM) {
+        this.largeurEnCM = largeurEnCM;
+        this.longueurEnCM = longueurEnCM;
+        this.hauteurEnCM = hauteurEnCM;
+    }
+
+    // Copy constructor
+    public Bureau(Bureau bureauACopier) {
+        this.largeurEnCM = bureauACopier.largeurEnCM;
+        this.longueurEnCM = bureauACopier.longueurEnCM;
+        this.hauteurEnCM = bureauACopier.hauteurEnCM;
+    }
+
+    // Method to position the desk at a specific height
+    public boolean PositionnerALaHauteur(BigDecimal hauteur) {
+        if (hauteur.compareTo(BigDecimal.ZERO) >= 0) {
+            this.hauteurEnCM = hauteur;
+            return true;
+        }
+        return false;
+    }
+
+    // Getter for hauteurEnCM
+    public BigDecimal getHauteurEnCM() {
+        return hauteurEnCM;
+    }
 }
