eadf0874dcea3ff09157e3d75b2dc7557cc8111f
KARAN PARIYAR <144631087+krn369@users.noreply.github.com>
2024-06-25 00:30:21 +0200

    Update App.java

--- 04 Extra/object/bouteille/src/bouteille/App.java
+++ 04 Extra/object/bouteille/src/bouteille/App.java
@@ -4,9 +4,31 @@ public class App {
 
 	public static void main(String[] args) {
 
-		// Make a few bottles
+        // Create a Coca bottle
+        Bouteille coca = new Bouteille(1.5f, 1.0f, false, "Coca");
+        System.out.println("Coca bottle created with " + coca.contenuEnL + "L of " + coca.typeDeContenu);
 
-		Bouteille bouteille1 = new Bouteille();
+        // Open the Coca bottle and fill it completely
+        coca.ouvrir();
+        coca.remplirComplet();
+        System.out.println("Coca bottle is now filled to " + coca.contenuEnL + "L");
+
+        // Create a H2O bottle
+        Bouteille h2o = new Bouteille();
+        System.out.println("H2O bottle created with " + h2o.contenuEnL + "L of " + h2o.typeDeContenu);
+
+        // Open the H2O bottle and empty it completely
+        h2o.ouvrir();
+        h2o.viderComplet();
+        System.out.println("H2O bottle is now emptied to " + h2o.contenuEnL + "L");
+
+        // Create a wine bottle
+        Bouteille wine = new Bouteille(0.75f, 0.75f, true, "Wine");
+        System.out.println("Wine bottle created with " + wine.contenuEnL + "L of " + wine.typeDeContenu);
+
+        // Empty some wine from the bottle
+        wine.vider(0.25f);
+        System.out.println("Wine bottle now contains " + wine.contenuEnL + "L after pouring out 0.25L");	
 
 	}
 
