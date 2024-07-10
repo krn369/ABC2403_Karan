cee12c5353447965e5fadf5f2b29247757a1165d
krn369 <144631087+krn369@users.noreply.github.com>
2024-07-10 12:09:22 +0200

    update

---
+++ 03-Algorithmes/04 Procédures et fonctions/01 Hello World/src/helloWorldMF/App.java
@@ -0,0 +1,14 @@
+package helloWorldMF;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		helloWorld();
+
+	}
+	
+	public static void helloWorld() {
+        System.out.println("Hello World!");
+	}
+}
